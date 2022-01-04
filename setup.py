922defad14e818b6e29af3cb3c90bdef27e419a6
N.G. Obot <nobot@acm.org>
2022-01-04 07:53:38 +0100

    built

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='ngobot_json',
     version='1.0.0',
     packages=['ngobot_json'],
-    url='https://github.com/ngobot/ngobot_json.git',
+    url='https://github.com/ngobot/ngobot_json',
     license='MIT License',
     author='Nseobong Gregory Obot',
     author_email='obot.greg@icloud.com',
